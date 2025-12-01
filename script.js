dad6b1e0ab83e0fe36f7bf8beb5c71a96b613fc7
Elijah Macharia <machariaelijah25@gmail.com>
2025-12-02 02:01:01 +0300

    Responsive nav-bar

---
+++ script.js
@@ -0,0 +1,72 @@
+// Scroll Logic
+
+// Selecting Elements
+const header = document.querySelector('.header');
+const scrollThreshold = 50;
+
+// Define Logic
+function toggleScrolledClass() {
+    if (window.scrollY > scrollThreshold) {
+        header.classList.add('header--scrolled');
+    } else {
+        header.classList.remove('header--scrolled');
+    }
+}
+
+//Attach Scroll Event
+window.addEventListener('scroll', toggleScrolledClass);
+
+// Check on Page load
+window.addEventListener('load', toggleScrolledClass);
+
+//Mobile Menu Logic
+// Select Elements
+const navToggle = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav-list');
+
+navToggle.addEventListener('click', () => {
+    navList.classList.toggle('is-open');
+
+    const isExpanded = navToggle.getAttribute(aria-expanded) === 'true';
+    navToggle.setAttribute('aria-expanded', !isExpanded);
+})
+
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('.nav-list a');
+
+const observerOptions = {
+    root: null,
+    rootMargin: '-20% 0px -70% 0px',
+    threshold: 0
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const id = entry.target.getAttribute('id');
+
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            const activeLink = document.querySelector(`.nav-list a[href="#${id}"]`);
+            if (activeLink) {
+                activeLink.classList.add('active');
+            }
+        }
+    });
+}, observerOptions);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navList.classList.remove('is-open');
+        navToggle.setAttribute('aria-expanded', 'false');
+    });
+});
+
+
+lucide.createIcons();
